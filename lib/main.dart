1772bc637420dab50627e5bf598937b1f87e29b5
Edo Cicak <edocicak@gmail.com>
2023-03-16 14:52:18 +0100

    Inicijalni commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text('NRS projekat'),
+        )
+        ),
+      );
+  }
+}
