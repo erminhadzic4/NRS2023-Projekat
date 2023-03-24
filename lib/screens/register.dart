303086e076779041a4aa33e446243a6b95bcb2e6
Ermin Hadzic <ehadzic4@etf.unsa.ba>
2023-03-24 21:45:30 +0100

    dodan ekran za validaciju broja mobitela

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
+import 'confirm_number.dart';
+
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
 
@@ -9,10 +11,16 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final List _focusInput = List.generate(6, (index) => FocusNode());
+  final _formkey = GlobalKey<FormState>();
+
+  String? _pendingPassword;
+
   InputDecoration registerInputDecoration(String labelText, String hintText) {
     return InputDecoration(
       isDense: true,
-      contentPadding: const EdgeInsets.only(bottom: 15, top: 15, left: 10, right: 10),
+      contentPadding:
+      const EdgeInsets.only(bottom: 15, top: 15, left: 10, right: 10),
       filled: true,
       fillColor: Colors.white,
       labelText: labelText,
@@ -26,60 +34,40 @@ class _RegisterState extends State<Register> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        //resizeToAvoidBottomInset: false,
         body: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const Padding(
-                padding: EdgeInsets.only(top: 80),
+                padding: EdgeInsets.only(top: 80, right: 200),
                 child: Text("Sign In",
                     style: TextStyle(
-                        color: Colors.black87,
+                        color: Colors.blueAccent,
                         fontSize: 45,
                         fontWeight: FontWeight.bold)),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 30),
                 child: Form(
+                  key: _formkey,
                   child: Column(
                     children: [
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 20),
                         child: TextFormField(
                           keyboardType: TextInputType.emailAddress,
-                          decoration: registerInputDecoration("Email", "Enter Email"),
-                          onChanged: (String value) {},
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: TextFormField(
-                          textCapitalization: TextCapitalization.words,
-                          keyboardType: TextInputType.name,
-                          decoration: registerInputDecoration("Name", "Enter Name"),
-                          onChanged: (String value) {},
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: TextFormField(
-                          textCapitalization: TextCapitalization.words,
-                          keyboardType: TextInputType.name,
-                          decoration: registerInputDecoration("Lat Name", "Enter Last Name"),
-                          onChanged: (String value) {},
+                          decoration:
+                          registerInputDecoration("Email", "Enter Email"),
+                          onFieldSubmitted: (String value) {
+                            FocusScope.of(context).requestFocus(_focusInput[0]);
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
                           validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter a password';
-                            } else if (value.length < 9) {
-                              return 'Your password needs to be longer than 9 characters';
+                            if (value == null ||
+                                value.isEmpty ||
+                                !value.contains("@")) {
+                              return 'Invalid address';
                             }
                             return null;
                           },
@@ -91,15 +79,21 @@ class _RegisterState extends State<Register> {
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 20),
                         child: TextFormField(
-                          obscureText: true,
-                          keyboardType: TextInputType.visiblePassword,
-                          decoration: registerInputDecoration("Password", "Enter Password"),
-                          onChanged: (String value) {},
+                          focusNode: _focusInput[0],
+                          textCapitalization: TextCapitalization.words,
+                          keyboardType: TextInputType.name,
+                          decoration:
+                          registerInputDecoration("Name", "Enter Name"),
+                          onFieldSubmitted: (String value) {
+                            FocusScope.of(context).requestFocus(_focusInput[1]);
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
                           validator: (value) {
                             if (value == null || value.isEmpty) {
-                              return 'Please enter a password';
-                            } else if (value.length < 9) {
-                              return 'Your password needs to be longer than 9 characters';
+                              return 'Name must contain at least one alphabetic character';
+                            } else if (value.contains(
+                                RegExp('[^a-zćčđšž]', caseSensitive: false))) {
+                              return 'Name cannot contain non-alphabetic characters';
                             }
                             return null;
                           },
@@ -111,11 +105,23 @@ class _RegisterState extends State<Register> {
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 20),
                         child: TextFormField(
-                          obscureText: true,
-                          keyboardType: TextInputType.visiblePassword,
-                          decoration: registerInputDecoration("Re-enter Password", "Enter your password again"),
+                          focusNode: _focusInput[1],
+                          textCapitalization: TextCapitalization.words,
+                          keyboardType: TextInputType.name,
+                          decoration: registerInputDecoration(
+                              "Last Name", "Enter Last Name"),
                           onChanged: (String value) {},
+                          onFieldSubmitted: (String value) {
+                            FocusScope.of(context).requestFocus(_focusInput[2]);
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
                           validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Name must contain at least one alphabetic character';
+                            } else if (value.contains(
+                                RegExp('[^a-zćčđšž]', caseSensitive: false))) {
+                              return 'Name cannot contain non-alphabetic characters';
+                            }
                             return null;
                           },
                         ),
@@ -126,10 +132,75 @@ class _RegisterState extends State<Register> {
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 20),
                         child: TextFormField(
+                          focusNode: _focusInput[2],
+                          obscureText: true,
                           keyboardType: TextInputType.visiblePassword,
-                          decoration: registerInputDecoration("Address", "Enter Address"),
-                          onChanged: (String value) {},
+                          decoration: registerInputDecoration(
+                              "Password", "Enter Password"),
+                          onChanged: (String value) {
+                            setState(() {
+                              _pendingPassword = value;
+                            });
+                          },
+                          onFieldSubmitted: (String value) {
+                            FocusScope.of(context).requestFocus(_focusInput[3]);
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
                           validator: (value) {
+                            if (value == null ||
+                                value.isEmpty ||
+                                value.length < 10) {
+                              return 'Password must be longer than 10 characters';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: TextFormField(
+                          focusNode: _focusInput[3],
+                          obscureText: true,
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: registerInputDecoration(
+                              "Re-enter Password", "Enter your password again"),
+                          onChanged: (String value) {},
+                          onFieldSubmitted: (String value) {
+                            FocusScope.of(context).requestFocus(_focusInput[4]);
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (value) {
+                            if (value == null ||
+                                value.isEmpty ||
+                                value != _pendingPassword) {
+                              return 'Passwords do not match';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: TextFormField(
+                          focusNode: _focusInput[4],
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: registerInputDecoration(
+                              "Address", "Enter Address"),
+                          onChanged: (String value) {},
+                          onFieldSubmitted: (String value) {
+                            FocusScope.of(context).requestFocus(_focusInput[5]);
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Invalid address';
+                            }
                             return null;
                           },
                         ),
@@ -140,7 +211,15 @@ class _RegisterState extends State<Register> {
                       Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 20),
                           child: InternationalPhoneNumberInput(
+                            focusNode: _focusInput[5],
                             onInputChanged: (PhoneNumber value) {},
+                            autoValidateMode: AutovalidateMode.onUserInteraction,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Invalid phone number';
+                              }
+                              return null;
+                            },
                           )),
                       const SizedBox(
                         height: 50,
@@ -151,10 +230,13 @@ class _RegisterState extends State<Register> {
                           elevation: 10,
                           height: 50,
                           minWidth: double.infinity,
-                          onPressed: () {},
+                          onPressed: () {
+                            if (_formkey.currentState!.validate()) {}
+                          },
                           color: Colors.blue,
                           child: const Text("Sign in",
                               style: TextStyle(
+                                fontSize: 20,
                                 color: Colors.white,
                                 fontWeight: FontWeight.bold,
                               )),
@@ -173,6 +255,7 @@ class _RegisterState extends State<Register> {
                           color: Colors.white,
                           child: const Text("Log In",
                               style: TextStyle(
+                                fontSize: 20,
                                 color: Colors.blue,
                                 fontWeight: FontWeight.bold,
                               )),
