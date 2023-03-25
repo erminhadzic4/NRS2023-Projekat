d8d925e82de20b3745a386eb1df84533a36bca44
Amer Mehmedić <harveyboarden@gmail.com>
2023-03-25 12:41:16 +0100

    Lista Transakcija
    Dodan screen sa listom transakcija

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:nrs2023/screens/register.dart';
+import 'package:nrs2023/screens/transactions.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Register();
+    return const Transactions();
   }
 }
---
+++ lib/screens/transactions.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Transactions extends StatelessWidget {
+  const Transactions({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class Transaction {
+  late String date;
+  late String type;
+  late double amount;
+
+  // constructor
+  Transaction(this.date, this.type, this.amount);
+}
+
+class _HomePageState extends State<HomePage> {
+  late List dummyList;
+  ScrollController _scrollController = ScrollController();
+  int _currentMax = 10;
+
+/*
+  @override
+  void initState() {
+    //TODO: implement initState
+    super.initState();
+    dummyList = List.generate(10, (index) => "Item : ${index + 1}");
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _getMoreList();
+      }
+    });
+  }
+  */
+
+  @override
+  void initState() {
+    //TODO: implement initState
+    super.initState();
+    dummyList = List.generate(
+      10,
+      (index) =>
+          Transaction('Jan $index 2021', 'Deposit', 100.0 + (index * 10)),
+    );
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _getMoreList();
+      }
+    });
+  }
+
+  /*
+  List<Transaction> transactions = [
+    Transaction('Jan 1, 2021','Deposit',100.0),
+    Transaction('Jan 2, 2021','Withdrawal',50.0),
+    Transaction('Jan 3, 2021','Payment',25.0),
+  ];
+  */
+
+  _getMoreList() {
+    for (int i = _currentMax; i < _currentMax + 10; i++) {
+      dummyList.add(Transaction('Jan $i 2021', 'Deposit', 100.0 + (i * 10)));
+    }
+    _currentMax = _currentMax + 10;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("All Transactions"),
+      ),
+      body: ListView.builder(
+        controller: _scrollController,
+        itemExtent: 80,
+        itemBuilder: (context, index) {
+          if (index == dummyList.length) {
+            return const CupertinoActivityIndicator();
+          }
+          return ListTile(
+            title: Text(dummyList[index].date),
+            subtitle: Text(dummyList[index].type),
+            trailing: Text(dummyList[index].amount.toString()),
+          );
+        },
+        itemCount: dummyList.length + 1,
+      ),
+    );
+  }
+}
