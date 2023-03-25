bf5bec126f07b379f316a91cab892bee12e1884c
Dajana Prašo <dpraso1@etf.unsa.ba>
2023-03-25 23:18:06 +0100

    spajanje pay i payment_page

--- lib/screens/pay.dart
+++ lib/screens/pay.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+
 class Pay extends StatefulWidget {
   @override
   _PayState createState() => _PayState();
+
 }
 
 class _PayState extends State<Pay> {
@@ -11,6 +13,7 @@ class _PayState extends State<Pay> {
   late String _transactionAmount;
   late String _recipientName;
   late String _recipientAccount;
+
 
   void _submitForm() {
     if (_formKey.currentState!.validate()) {
@@ -31,6 +34,51 @@ class _PayState extends State<Pay> {
         ),
       );
     }
+  }
+
+  bool validatePayInfo(String amount, String name, String account) {
+    // Check if all fields are non-empty
+    if (amount.isEmpty || name.isEmpty || account.isEmpty) {
+      return false;
+    }
+
+    // Check if the amount is a valid number
+    try {
+      double.parse(amount);
+    } catch (e) {
+      return false;
+    }
+
+    // Check if the account number is a valid format (e.g., 10 digits)
+    if (account.length != 10) {
+      return false;
+    }
+
+    // All checks passed, the information is valid
+    return true;
+  }
+
+  void Pay(BuildContext context) {
+    // Validate the pay information
+    if (validatePayInfo(_transactionAmount, _recipientName, _recipientName)) {
+      // Navigate to the PaymentPage
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => PaymentPage()),
+      );
+    } else {
+      // Display an error message
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Invalid pay information')),
+      );
+    }
+  }
+
+  void _goToPaymentPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => PaymentPage()),
+    );
   }
 
   @override
@@ -98,10 +146,135 @@ class _PayState extends State<Pay> {
               ),
               SizedBox(height: 16.0),
               ElevatedButton(
-                onPressed: _submitForm,
+                onPressed: _goToPaymentPage,
                 child: Text('Submit'),
               ),
             ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class PaymentPage extends StatefulWidget {
+  @override
+  _PaymentPageState createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _amountController = TextEditingController();
+  final TextEditingController _recipientNameController =
+  TextEditingController();
+  final TextEditingController _recipientAccountController =
+  TextEditingController();
+  String _selectedCurrency = "USD";
+  final List<String> _currencies = ['USD', 'AUD', 'BRL', 'CAD', 'CHF', 'CZK', 'DKK', 'EUR', 'GBP', 'HKD', 'HUF', 'ILS', 'JPY', 'MXN', 'NOK', 'NZD', 'PHP', 'PLN', 'RUB', 'SEK', 'SGD', 'THB', 'TWD'];
+
+  void _submitPaymentForm() {
+    if (_formKey.currentState!.validate()) {
+      if (_recipientNameController.text.isEmpty ||
+          _recipientAccountController.text.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Recipient name or account details are required.'),
+        ));
+      } else {
+
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text('Payment'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Amount'),
+                TextFormField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (_amountController.text.isNotEmpty &&
+                        double.tryParse(_amountController.text) == 0.0) {
+                      return 'Amount is required';
+                    } else if (double.tryParse(_amountController.text) ==
+                        null) {
+                      return 'Amount should be a valid number.';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    suffixText: _selectedCurrency,
+                    suffixStyle:
+                    TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    hintText: '0.00',
+                  ),
+                ),
+                SizedBox(height: 16),
+                Text('Currency'),
+                DropdownButtonFormField<String>(
+                  value: _selectedCurrency,
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedCurrency = value!;
+                    });
+                  },
+                  items: _currencies
+                      .map((currency) => DropdownMenuItem(
+                    value: currency,
+                    child: Text(currency),
+                  ))
+                      .toList(),
+                ),
+                SizedBox(height: 16),
+                Text('Recipient Name'),
+                TextFormField(
+                  controller: _recipientNameController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Recipient name is required';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Enter recipient name',
+                  ),
+                ),
+                SizedBox(height: 16),
+                Text('Recipient Account'),
+                TextFormField(
+                  controller: _recipientAccountController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Recipient account details are required';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Enter recipient account details',
+                  ),
+                ),
+                SizedBox(height: 16),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: _submitPaymentForm,
+                    child: Text('Submit'),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
