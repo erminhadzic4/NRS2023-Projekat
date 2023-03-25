181e3906255c9231d1e269d28012a5619279c82d
ofbukurevic <omer.faruk.bukurevic@gmail.com>
2023-03-25 13:16:19 +0100

    Home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:nrs2023/screens/homeScreen.dart';
 import 'package:nrs2023/screens/register.dart';
 import 'package:nrs2023/screens/confirm_number.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: Register());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
   }
 }
