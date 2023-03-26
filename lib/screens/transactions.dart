ba8ee8be50306981751c6d7e31f1dcbacc54fde1
Amer Mehmedić <harveyboarden@gmail.com>
2023-03-26 13:44:39 +0200

    Nova lista transakcija

--- lib/screens/transactions.dart
+++ lib/screens/transactions.dart
@@ -2,30 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nrs2023/screens/transactionDetails.dart';
 
-class Transactions extends StatelessWidget {
-  const Transactions({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
 class Transaction {
   late String date;
   late String type;
@@ -39,34 +15,24 @@ class Transaction {
       this.date, this.type, this.amount, this.currency, this.details, this.id);
 }
 
-class _HomePageState extends State<HomePage> {
-  late List dummyList;
+class Transactions extends StatefulWidget {
+  @override
+  InitalState createState() => InitalState();
+}
+
+class InitalState extends State<Transactions> {
+  late List transactions;
   ScrollController _scrollController = ScrollController();
-  int _currentMax = 10;
-
-/*
-  @override
-  void initState() {
-    //TODO: implement initState
-    super.initState();
-    dummyList = List.generate(10, (index) => "Item : ${index + 1}");
-    _scrollController.addListener(() {
-      if (_scrollController.position.pixels ==
-          _scrollController.position.maxScrollExtent) {
-        _getMoreList();
-      }
-    });
-  }
-  */
+  int _currentMax = 8;
 
   @override
   void initState() {
-    //TODO: implement initState
+    //TODO: implement initState from Backend
     super.initState();
-    dummyList = List.generate(
-      10,
-      (index) => Transaction('Jan $index 2021', 'Deposit', 100.0 + (index * 10),
-          'EUR', 'detail', '12345'),
+    transactions = List.generate(
+      8,
+      (index) => Transaction('Jan ${index + 1} 2021', 'Deposit',
+          100.0 + (index * 10), 'EUR', 'detail', '12345'),
     );
     _scrollController.addListener(() {
       if (_scrollController.position.pixels ==
@@ -76,20 +42,12 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  /*
-  List<Transaction> transactions = [
-    Transaction('Jan 1, 2021','Deposit',100.0),
-    Transaction('Jan 2, 2021','Withdrawal',50.0),
-    Transaction('Jan 3, 2021','Payment',25.0),
-  ];
-  */
-
   _getMoreList() {
     for (int i = _currentMax; i < _currentMax + 10; i++) {
-      dummyList.add(Transaction('Jan $i 2021', 'Deposit', 100.0 + (i * 10),
-          'EUR', 'detail', '12345'));
+      transactions.add(Transaction('Jan ${i + 1} 2021', 'Deposit',
+          100.0 + (i * 10), 'EUR', 'detail', '12345'));
     }
-    _currentMax = _currentMax + 10;
+    _currentMax = _currentMax + 8;
     setState(() {});
   }
 
@@ -101,32 +59,32 @@ class _HomePageState extends State<HomePage> {
       ),
       body: ListView.builder(
         controller: _scrollController,
-        itemExtent: 80,
+        itemExtent: 85,
         itemBuilder: (context, index) {
-          if (index == dummyList.length) {
+          if (index == transactions.length) {
             return const CupertinoActivityIndicator();
           }
           return ListTile(
-            title: Text(dummyList[index].date),
-            subtitle: Text(dummyList[index].type),
-            trailing: Text(dummyList[index].amount.toString()),
+            title: Text(transactions[index].date),
+            subtitle: Text(transactions[index].type),
+            trailing: Text(transactions[index].amount.toString()),
             onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => TransactionDetailsScreen(
-                      transactionId: dummyList[index].id,
-                      transactionCurrency: dummyList[index].currency,
-                      transactionType: dummyList[index].type,
-                      transactionAmount: dummyList[index].amount,
-                      transactionDate: dummyList[index].date,
-                      transactionDetails: dummyList[index].details),
+                      transactionId: transactions[index].id,
+                      transactionCurrency: transactions[index].currency,
+                      transactionType: transactions[index].type,
+                      transactionAmount: transactions[index].amount,
+                      transactionDate: transactions[index].date,
+                      transactionDetails: transactions[index].details),
                 ),
               );
             },
           );
         },
-        itemCount: dummyList.length + 1,
+        itemCount: transactions.length + 1,
       ),
     );
   }
