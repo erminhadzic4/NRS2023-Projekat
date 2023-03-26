5af14514ab989ff5d33f2bce4cc5c2a9cf4af1e5
Amir <ahastor@etf.unsa.ba>
2023-03-26 16:02:25 +0200

    dodan kod za povlacenje tranzakcija sa API-a

--- lib/screens/transactions.dart
+++ lib/screens/transactions.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nrs2023/screens/transactionDetails.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Transaction {
   late String date;
@@ -13,6 +15,17 @@ class Transaction {
   // constructor
   Transaction(
       this.date, this.type, this.amount, this.currency, this.details, this.id);
+
+  factory Transaction.fromJson(Map<String, dynamic> json) {
+    return Transaction(
+      json['date'],
+      json['type'],
+      json['amount'].toDouble(),
+      json['id'],
+      json['currency'],
+      json['details'],
+    );
+  }
 }
 
 class Transactions extends StatefulWidget {
@@ -20,17 +33,38 @@ class Transactions extends StatefulWidget {
   InitalState createState() => InitalState();
 }
 
-class InitalState extends State<Transactions> {
-  late List transactions;
-  ScrollController _scrollController = ScrollController();
-  int _currentMax = 8;
 
+class InitalState extends State<Transactions> {
+  late List<Transaction> transactions;
+  ScrollController _scrollController = ScrollController();
+  int _currentMax = 10;
+  int _currentPage = 1;
+  bool _isLoading = false;
+
+//KOD Za povlacenje tranzakcija sa API-a
+
+/*
+  @override
+  void initState() {
+    super.initState();
+    transactions = [];
+    _getMoreTransactions();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _getMoreTransactions();
+      }
+    });
+  }
+*/
+
+//KOD za dummy podatke
   @override
   void initState() {
     //TODO: implement initState from Backend
     super.initState();
     transactions = List.generate(
-      8,
+      10,
       (index) => Transaction('Jan ${index + 1} 2021', 'Deposit',
           100.0 + (index * 10), 'EUR', 'detail', '12345'),
     );
@@ -47,9 +81,34 @@ class InitalState extends State<Transactions> {
       transactions.add(Transaction('Jan ${i + 1} 2021', 'Deposit',
           100.0 + (i * 10), 'EUR', 'detail', '12345'));
     }
-    _currentMax = _currentMax + 8;
+    _currentMax = _currentMax + 10;
     setState(() {});
   }
+//KOD za dummy podatke
+
+
+//KOD za podatke sa servera
+  Future<void> _getMoreTransactions() async {
+    if (_isLoading) {
+      return;
+    }
+    _isLoading = true;
+    //URL ce se promijeniti kada RI završti backend
+    final url = Uri.parse('https://my-api.com/transactions?page=$_currentPage');
+    final response = await http.get(url);
+    final responseData = json.decode(response.body);
+    final List<Transaction> loadedTransactions = [];
+    responseData['data'].forEach((transactionData) {
+      loadedTransactions.add(Transaction.fromJson(transactionData));
+    });
+    setState(() {
+      transactions.addAll(loadedTransactions);
+      _isLoading = false;
+      _currentPage++;
+    });
+  }
+//KOD za podatke sa servera
+
 
   @override
   Widget build(BuildContext context) {
