d98114f3d78f25a9af341e1a990f9ec198747553
Edo Cicak <edocicak@gmail.com>
2023-04-01 16:09:39 +0200

    Popravljen input za account. Treba dodati da se tamplates mogu editovati, brisati i koristiti za tranzakcije.

--- lib/screens/templates.dart
+++ lib/screens/templates.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
 class Payment {
   TextEditingController Currency = TextEditingController();
   TextEditingController? Amount = TextEditingController();
@@ -21,6 +20,25 @@ class TemplatesPage extends StatefulWidget {
   _TemplatesPageState createState() => _TemplatesPageState();
 }
 
+class _AccountNumberFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    String value = newValue.text.replaceAll('-', '');
+    String formattedValue = '';
+    for (int i = 0; i < value.length; i++) {
+      formattedValue += value[i];
+      if ((i + 1) % 4 == 0 && i != value.length - 1) {
+        formattedValue += '-';
+      }
+    }
+    return TextEditingValue(
+      text: formattedValue,
+      selection: TextSelection.collapsed(offset: formattedValue.length),
+    );
+  }
+}
+
 class _TemplatesPageState extends State<TemplatesPage> {
   List<Payment> templates = [];
 
@@ -29,9 +47,9 @@ class _TemplatesPageState extends State<TemplatesPage> {
   final TextEditingController _currencyController = TextEditingController();
   final TextEditingController _amountController = TextEditingController();
   final TextEditingController _recipientNameController =
-  TextEditingController();
+      TextEditingController();
   final TextEditingController _recipientAccountController =
-  TextEditingController();
+      TextEditingController();
   String? _selectedCurrency = "USD";
   final List<String> _currencies = [
     'USD',
@@ -58,26 +76,7 @@ class _TemplatesPageState extends State<TemplatesPage> {
     'THB',
     'TWD'
   ];
-  /*void _addTemplate() {
-    if (_formKey.currentState!.validate()) {
-      Payment template = Payment(
-        Currency: _currencyController,
-        Amount: _amountController.text.isEmpty
-            ? null
-            : _amountController, // make it nullable if the user left it empty
-        RecipientName: _recipientNameController,
-        RecipientAccount: _recipientAccountController,
-      );
-      setState(() {
-        templates.add(template);
-      });
-      _currencyController.clear();
-      _amountController.clear();
-      _recipientNameController.clear();
-      _recipientAccountController.clear();
-    }
-  }
-*/
+
   @override
   void dispose() {
     _currencyController.dispose();
@@ -86,24 +85,24 @@ class _TemplatesPageState extends State<TemplatesPage> {
     _recipientAccountController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Payment Templates'),
-      ), body: ListView.builder(
-      itemCount: templates.length,
-      itemBuilder: (BuildContext context, int index) {
-        if (templates.length != 0) {
-          return ListTile(
-            title: Text(
-                templates[index].RecipientName!.text.toString()),
-            subtitle: Text(
-                templates[index].Amount?.text.toString() ?? 'N/A'),
-          );
-        }
-      },
-    ),
+      ),
+      body: ListView.builder(
+        itemCount: templates.length,
+        itemBuilder: (BuildContext context, int index) {
+          if (templates.length != 0) {
+            return ListTile(
+              title: Text(templates[index].RecipientName!.text.toString()),
+              subtitle: Text(templates[index].Amount?.text.toString() ?? 'N/A'),
+            );
+          }
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
@@ -136,7 +135,8 @@ class _TemplatesPageState extends State<TemplatesPage> {
                         ),
                         TextFormField(
                           controller: _amountController,
-                          keyboardType: TextInputType.numberWithOptions(decimal: true),
+                          keyboardType:
+                              TextInputType.numberWithOptions(decimal: true),
                           inputFormatters: [
                             FilteringTextInputFormatter.allow(
                                 RegExp(r'^\d+\.?\d{0,2}')),
@@ -144,26 +144,30 @@ class _TemplatesPageState extends State<TemplatesPage> {
                           validator: (value) {
                             if (_amountController.text.isEmpty) {
                               return 'Amount is required';
-                            } else if (double.tryParse(_amountController.text) ==
+                            } else if (double.tryParse(
+                                    _amountController.text) ==
                                 null) {
                               return 'Amount should be a valid number.';
-                            } else if (double.parse(_amountController.text) > 100000) {
+                            } else if (double.parse(_amountController.text) >
+                                100000) {
                               return 'Amount cannot be greater than 100 000!';
-                            } else if(double.parse(_amountController.text) == 0) {
+                            } else if (double.parse(_amountController.text) ==
+                                0) {
                               return 'Amount cannot be 0!';
                             }
                             return null;
                           },
                           decoration: InputDecoration(
                             suffixText: _selectedCurrency,
-                            suffixStyle:
-                            TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                            suffixStyle: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
                             hintText: '0.00',
                           ),
                         ),
                         TextFormField(
                           controller: _recipientNameController,
-                          decoration: InputDecoration(labelText: 'Recipient name'),
+                          decoration:
+                              InputDecoration(labelText: 'Recipient name'),
                           validator: (value) {
                             if (value!.isEmpty) {
                               return 'Recipient name is required';
@@ -175,7 +179,13 @@ class _TemplatesPageState extends State<TemplatesPage> {
                         ),
                         TextFormField(
                           controller: _recipientAccountController,
-                          decoration: InputDecoration(labelText: 'Recipient account'),
+                          decoration:
+                              InputDecoration(labelText: 'Recipient account'),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.allow(RegExp(r'[\d-]')),
+                            _AccountNumberFormatter(),
+                            LengthLimitingTextInputFormatter(19),
+                          ],
                           validator: (value) {
                             if (value!.isEmpty) {
                               return 'Recipient account details are required';
