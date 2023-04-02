4130cae4d280c3da4c1c22258626eed73caa1d12
obukurevic1 <obukurevic1@etf.unsa.ba>
2023-04-02 15:50:52 +0200

    homescreen iz main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginAuthScreen(),
+      home: HomeScreen(),
     );
   }
 }
