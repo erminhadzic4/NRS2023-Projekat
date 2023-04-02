e7757dce53ddb9e3667141cc48f2fc019c331a76
fjaganjac <fjaganjac1@etf.unsa.ba>
2023-04-02 12:38:57 +0200

    povezan backend sa validacijom email-a

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -350,8 +350,7 @@ class _RegisterState extends State<Register> {
                                     _controllers[3].text, //username
                                     _controllers[4].text,
                                     _controllers[5].text,
-                                    "06${_controllers[6].text}");
-                                print(_controllers[6].text);
+                                    "0${_controllers[6].text}");
                               }
                             }),
                       ),
