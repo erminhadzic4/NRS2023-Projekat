32567113033a0d8a88744d88b8444a981ad2eee5
fjaganjac <fjaganjac1@etf.unsa.ba>
2023-04-11 22:42:36 +0200

    implementirano prikazivanje notifikacija

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,20 @@ import 'package:nrs2023/screens/loginAuth.dart';
 import 'package:nrs2023/screens/numberValidation.dart';
 import 'package:nrs2023/screens/home.dart';
 import 'package:nrs2023/screens/welcome.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 
 void main() {
+  AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+        channelKey: 'channelKey',
+        channelName: 'channelName',
+        channelDescription: 'channelDescription',
+    )
+    ],
+    debug :true,
+  );
   runApp(MyApp());
 }
 
