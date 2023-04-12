28c233cb9cb2e60d7a0ca7edff0c71bccf7bdaa8
afrljak1 <afrljak1@etf.unsa.ba>
2023-04-12 15:20:53 +0200

    Ureden layout za biometrics login

--- lib/screens/biometricAuth.dart
+++ lib/screens/biometricAuth.dart
@@ -60,7 +60,77 @@ class BiometricAuthenticationState extends State<BiometricAuthentication> {
     }
   }
 
+
+
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(
+              child: Text(
+                "Biometrics Login",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+              margin: EdgeInsets.symmetric(vertical: 50.0),
+              child: Column(
+                children: [
+                  Image.asset(
+                    "res/img/fingerprint.jpg",
+                    width: 140.0,
+                  ),
+                /*  Text(
+                    "Fingerprint Auth",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+
+                    ),
+                  ),*/
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 15.0),
+                    child: const Text(
+                      "Authenticate using your fingerprint",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, height: 1.5),
+                    ),
+                  ),
+
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 15.0),
+                    width: 400,
+                    child: ElevatedButton(
+                        onPressed: authenticate,
+                        child: Text("Authenticate", style: TextStyle(color: Colors.white,
+                            fontSize: 20.0),
+                        )
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+@override
   Widget build(BuildContext context) {
     return  Scaffold(
         appBar: AppBar(
@@ -83,3 +153,4 @@ class BiometricAuthenticationState extends State<BiometricAuthentication> {
     );
   }
 }
+*/
