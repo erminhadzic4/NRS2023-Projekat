660d2ee6a649cd25d32c3dbadce2b71c839f315d
Dajana Prašo <dpraso1@etf.unsa.ba>
2023-05-10 15:29:07 +0200

    lista korisnikovih accounta

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:local_auth/local_auth.dart';
+import 'package:nrs2023/screens/accountList.dart';
 import 'package:nrs2023/screens/pay.dart';
 import 'package:nrs2023/screens/register.dart';
 import 'package:nrs2023/screens/accountCreation.dart';
@@ -226,12 +227,10 @@ class _HomeScreen extends State<HomeScreen> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => const PaymentPage(
-                      templateData: ["", "", "", ""],
-                      recipientName: '',
-                      recipientAccount: '',
-                      amount: '',
-                      currency: '',
+                    builder: (context) => AccountListPage(
+                      currency: ["", "", "", ""],
+                      bankName: '',
+                      description: '',
                     )),
               );
               break;
