d4587c0c9d4a9f4848a33e470a6814840278142a
aatlic1 <aatlic1@etf.unsa.ba>
2023-05-10 16:04:57 +0200

    Ispravak

--- lib/screens/claim.dart
+++ lib/screens/claim.dart
@@ -9,14 +9,12 @@ class Claim {
   String description;
   File file;
   DateTime dateTime;
-  String status;
 
   Claim(
     this.subject,
     this.description,
     this.file,
     this.dateTime,
-    this.status,
   );
 
 //   factory Transaction.fromJson(Map<String, dynamic> json) {
@@ -42,7 +40,6 @@ class ClaimPage extends StatefulWidget {
 }
 
 String path = "";
-String status = "Open";
 
 class _ClaimPageState extends State<ClaimPage> {
   TextEditingController _problemTypeController = TextEditingController();
@@ -69,31 +66,6 @@ class _ClaimPageState extends State<ClaimPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Visibility(
-              visible: status == 'Open',
-              //visible: claim.status == 'open',
-              child: Container(
-                width: 400,
-                height: 40,
-                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1.0,
-                  ),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Text(
-                  'Status: $status',
-                  //'Status: ${claim.status}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 16.0),
             TextField(
               controller: _problemTypeController,
               decoration: InputDecoration(
