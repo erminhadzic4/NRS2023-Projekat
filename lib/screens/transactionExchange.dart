46a1902baf64162d8e036ae09cea5fc8e5bd79e0
Zaidan Maarouf <zmaarouf1@etf.unsa.ba>
2023-05-23 17:07:59 +0200

    popravljeno sve

--- lib/screens/transactionExchange.dart
+++ lib/screens/transactionExchange.dart
@@ -22,9 +22,10 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
     _accounts.clear();
     String? token = await storage.read(key: 'token');
 
-
+    print(token);
     final getAccounts = await http.get(
-      Uri.parse('http://siprojekat.duckdns.org:5051/api/Account/user-accounts'),
+      Uri.parse(
+          'http://siprojekat.duckdns.org:5051/api/Exchange/GetUserAccounts'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=utf-8',
         'authorization': 'Bearer $token'
@@ -32,86 +33,66 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
     );
 
     var accountsList = json.decode(getAccounts.body);
+    // print(accountsList);
 
     for (int i = 0; i < accountsList.length; i++) {
-      String currency = accountsList[i]['currency']['name'];
-      String bankName = accountsList[i]['description'];
-      String accountNumber = accountsList[i]['account'];
+      String currency = accountsList[i]['currency'];
+      String bankName = accountsList[i]['bankName'];
+      String accountNumber = accountsList[i]['accountNumber'];
 
       var account = Accounts(
         currency: currency,
         bankName: bankName,
         account: accountNumber,
       );
+      //   print(account);
       _accounts.add(account);
     }
   }
+
   final _purpose = TextEditingController();
   double _exchangeRate = 1.0;
   double _amountToExchange = 0.0;
   String _selectedCurrency = 'USD';
-  final List<String> _currencies = [
-    'USD',
-    'AUD',
-    'BRL',
-    'CAD',
-    'CHF',
-    'CZK',
-    'DKK',
-    'EUR',
-    'GBP',
-    'HKD',
-    'HUF',
-    'ILS',
-    'JPY',
-    'MXN',
-    'NOK',
-    'NZD',
-    'PHP',
-    'PLN',
-    'RUB',
-    'SEK',
-    'SGD',
-    'THB',
-    'TWD'
-  ];
+  final List<String> _currencies = ['USD', 'BAM', 'CHF', 'EUR'];
 
   List<String> _availableCurrencies = [];
 
   @override
   void initState() {
     super.initState();
-    //fetchCurrencies();
+    fetchCurrencies();
   }
-
 
   Future<void> fetchCurrencies() async {
     String? token = await storage.read(key: 'token');
+    //  print(token);
     try {
       final response = await http.get(
-        Uri.parse("http://siprojekat.duckdns.org:5051/api/ExchangeRate/currency"),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-          'Authorization': 'Bearer $token'
-        },
-      );
-
+          Uri.parse(
+              "http://siprojekat.duckdns.org:5051/api/ExchangeRate/currency"),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+            'authorization': 'Bearer $token'
+          });
+      //   print(json.decode(response.body));
       if (response.statusCode == 200) {
         final responseData = json.decode(response.body);
         final currencies = responseData as List<dynamic>;
         setState(() {
           _availableCurrencies = currencies.cast<String>();
           _selectedCurrency = _currencies[0];
+          //  print(_selectedCurrency);
         });
         await getExchangeRate();
-
       } else {
         // Handle error response
         showDialog(
           context: context,
           builder: (context) => AlertDialog(
             title: Text('Error'),
-            content: Text('Failed to fetch the available currencies: $_availableCurrencies'),
+            content: Text(
+                'Failed to fetch the available currencies: $_availableCurrencies'),
             actions: [
               TextButton(
                 onPressed: () {
@@ -124,12 +105,14 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
         );
       }
     } catch (e) {
+      //print(e);
       // Handle network or other errors
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
           title: Text('Error'),
-          content: Text('An error occurred while fetching the available currencies.'),
+          content: Text(
+              'An error occurred while fetching the available currencies.'),
           actions: [
             TextButton(
               onPressed: () {
@@ -151,19 +134,19 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
     };
 
     final getAccounts = await http.get(
-      Uri.parse('http://siprojekat.duckdns.org:5051/api/Exchange/GetUserAccounts'),
+      Uri.parse(
+          'http://siprojekat.duckdns.org:5051/api/Exchange/GetUserAccounts'),
       headers: headers,
     );
 
     final accountsList = json.decode(getAccounts.body);
     if (accountsList.isNotEmpty) {
-      final currency = accountsList[0]['currency']['name'];
+      final currency = accountsList[0]['currency'];
+      print(currency);
       return currency.toString();
     }
     return null; // Return null when the user has no accounts
-
   }
-
 
   Future<void> getExchangeRate() async {
     String? token = await storage.read(key: 'token');
@@ -171,7 +154,8 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
 
     try {
       final response = await http.get(
-        Uri.parse("http://siprojekat.duckdns.org:5051/api/ExchangeRate?inputCurreny=$userCurrency&outputCurrency=$_selectedCurrency"),
+        Uri.parse(
+            "http://siprojekat.duckdns.org:5051/api/ExchangeRate?inputCurreny=$userCurrency&outputCurrency=$_selectedCurrency"),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
           'Authorization': 'Bearer $token'
@@ -179,7 +163,11 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
       );
       if (response.statusCode == 200) {
         final responseData = json.decode(response.body);
-        final exchangeRate = responseData['rate'] as double;
+
+        print(responseData);
+
+        final exchangeRate = responseData[0]['rate'];
+        print(exchangeRate);
 
         setState(() {
           _exchangeRate = exchangeRate;
@@ -203,6 +191,7 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
         );
       }
     } catch (e) {
+      print(e);
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
@@ -221,16 +210,16 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
     }
   }
 
-
   Future<void> createExchangeTransaction() async {
     String? token = await storage.read(key: 'token');
     var account = await getUserAccounts();
+
     String? userCurrency = await getUserCurrency();
 
     try {
-
       final response = await http.post(
-          Uri.parse("http://siprojekat.duckdns.org:5051/api/Exchange/CreateExchangeTransaction"),
+          Uri.parse(
+              "http://siprojekat.duckdns.org:5051/api/Exchange/CreateExchangeTransaction"),
           headers: <String, String>{
             'Content-Type': 'application/json; charset=UTF-8',
             'Authorization': 'Bearer $token'
@@ -239,10 +228,11 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
             "amount": _amountToExchange,
             "transactionPurpose": _purpose.text,
             "senderCurrency": userCurrency,
-            "recipientCurrency": _selectedCurrency,
+            "recipientCurrnecy": _selectedCurrency,
           }));
       print(response.statusCode);
 
+      print(response.body);
 
       if (response.statusCode == 200) {
         // Transaction successful
@@ -281,12 +271,14 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
         );
       }
     } catch (e) {
+      print(e);
       // Handle network or other errors
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
           title: Text('Error'),
-          content: Text('An error occurred while creating the exchange transaction.'),
+          content: Text(
+              'An error occurred while creating the exchange transaction.'),
           actions: [
             TextButton(
               onPressed: () {
@@ -299,8 +291,6 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
       );
     }
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -334,21 +324,20 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
                   items: _currencies
                       .map(
                         (currency) => DropdownMenuItem(
-                      value: currency,
-                      child: Text(currency),
-                    ),
-                  )
+                          value: currency,
+                          child: Text(currency),
+                        ),
+                      )
                       .toList(),
                 ),
-
-
                 SizedBox(height: 16),
                 Text('Amount to exchange: $_amountToExchange'),
                 TextFormField(
                   controller: _amountController,
                   keyboardType: TextInputType.numberWithOptions(decimal: true),
                   inputFormatters: [
-                    FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d{0,2}')),
+                    FilteringTextInputFormatter.allow(
+                        RegExp(r'^\d+\.?\d{0,2}')),
                   ],
                   onChanged: (value) {
                     setState(() {
@@ -369,7 +358,8 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
                   },
                   decoration: InputDecoration(
                     suffixText: _selectedCurrency,
-                    suffixStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    suffixStyle:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     hintText: '0.00',
                   ),
                 ),
@@ -380,7 +370,8 @@ class _TransactionExchangeState extends State<TransactionExchangePage> {
                   ),
                 ),
                 SizedBox(height: 16),
-                Text('Exchange rate (Current currency to $_selectedCurrency): $_exchangeRate '),
+                Text(
+                    'Exchange rate (Current currency to $_selectedCurrency): $_exchangeRate '),
                 ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
