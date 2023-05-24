f0ce16f5744a3c10496d341451031b6578287b56
Amir Hastor <amir.hastor@gmail.com>
2023-05-24 02:04:38 +0200

    testovi v2

--- integration_test/transactions_test.dart
+++ integration_test/transactions_test.dart
@@ -6,38 +6,36 @@ import 'package:nrs2023/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   testWidgets('Test', (WidgetTester tester) async {
-    // Run the app
     app.main();
+
     await tester.pumpAndSettle();
 
-    // Find the log in button by its key
     final logInButton = find.byKey(const ValueKey('logInButton'));
-
-    // Tap the log in button
     await tester.tap(logInButton);
-    await Future.delayed(const Duration(seconds: 3));
     await tester.pumpAndSettle();
 
-    // Enter valid credentials in the username and password fields.
-    await tester.enterText(
-        find.byKey(const ValueKey('EmailField')), 'damke23@gmail.com');
-    await tester.enterText(
-        find.byKey(const ValueKey('PasswordField')), 'Jabuka32!');
+    await tester.enterText(find.byKey(const ValueKey('EmailField')), 'damke23@gmail.com');
+    await tester.enterText(find.byKey(const ValueKey('PasswordField')), 'Jabuka32!');
+    await tester.pumpAndSettle();
 
-    // Scroll down until LOGIN button is visible
     await tester.dragUntilVisible(
       find.text('LOGIN'),
       find.byKey(const ValueKey('Container')),
       const Offset(-250, 0),
     );
-    await Future.delayed(const Duration(seconds: 1));
-    // Tap the login button.
     await tester.tap(find.byKey(const ValueKey('loginbutton')));
     await tester.pumpAndSettle();
-    await Future.delayed(const Duration(seconds: 8));
-    // Tap OK
-    await tester.tap(find.text('OK'));
+
+    // Wait for the dialog box to appear
+    await tester.pumpAndSettle(const Duration(seconds: 2));
+
+    // Find and tap the 'OK' button on the dialog
+    final okButton = find.text('OK');
+    await tester.tap(okButton);
     await tester.pumpAndSettle();
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
   });
 }
